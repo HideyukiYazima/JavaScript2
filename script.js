e00ec05940e588bcc4bdd077f32d1faa15272ce8
Yazima <yazima04200@gmail.com>
2022-12-09 23:52:00 +0900

    変数計算

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-let name = "
+let name = "hideyuki";
+
+console.log("name");
+
+name = "yajima";
+
+console.log("name");
+
+let number = 2;
+// 2
+
+number = number + 3;
+// 5
+
+console.log(number);
+
+let number = 7;
+console.log(number);
+
+// +3する
+number = number + 3;
+
+console.log(number);
+
+// 割る2する
+number /= 2;
+
+console.log(number);
