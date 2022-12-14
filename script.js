2717462f6a92e9e589471e7b4da8684c409641fe
Yazima <yazima04200@gmail.com>
2022-12-14 21:43:22 +0900

    if文　&&

--- script.js
+++ script.js
@@ -42,3 +42,46 @@ const level = 12;
 if (level > 10) {
   console.log("レベルが10より大きいです");
 }
+
+//
+const password = "矢嶋";
+
+if (password === "矢嶋") {
+  console.log("ログインに成功しました");
+}
+
+if (password !== "矢嶋") {
+  console.log("パスワードが間違っています");
+}
+
+//
+const number = 19;
+
+if (number >= 20) {
+  console.log("20以上です");
+}
+
+else {
+  console.log("20以下です");
+}
+
+// else if
+const number = 19;
+
+if (number >= 20) {
+  console.log("私の番号は20以上です");
+}
+
+else if (number >= 10) {
+  console.log("私の番号は20以下ですが、10以上です");
+}
+
+else {
+  console.log("私の番号は10未満です");
+}
+
+// 〇以上かつ〇未満
+const number = 23;
+if (number >= 20 && number < 30) {
+  console.log("私の番号は20番代です");
+}
